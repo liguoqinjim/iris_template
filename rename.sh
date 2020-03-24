660619e2389c5e531553e18a57402d20b2e66072
liguoqinjim <liguoqinjim23@gmail.com>
2020-03-24 20:40:44 +0800

    update

--- rename.sh
+++ rename.sh
@@ -2,12 +2,12 @@
 
 echo 'new module name:'$1
 
-
-#find . -name '*.go' -print0 | xargs -0 sed -i "" "s/form/forms/g"
-
 # -e 测试
 #origin="github.com/liguoqinjim/iris_template"
 #sed -e 's,'"$origin"','"$1"',g' "web/controller/user.go"
+
+origin="github.com/liguoqinjim/iris_template"
+# sed -i '' 's,'"$a"','"$1"',g' "web/controller/user.go"
 
 # 修改所有go文件
 find . -name '*.go' -print0 | xargs -0 sed -i '' 's,'"$origin"','"$1"',g'
