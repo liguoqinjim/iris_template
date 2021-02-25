326a4edb00ddaf2ebf5cfa7a49abef0edf8c6871
liguoqinjim <liguoqinjim23@gmail.com>
2021-02-25 14:37:44 +0800

    new.sh

--- new.sh
+++ new.sh
@@ -9,15 +9,15 @@ fi
 
 # 拷贝至新目录
 # 忽略.idea和.git目录
-rsync -av --exclude .idea --exclude .git --exclude log --exclude logs --exclude releases --exclude app.toml --exclude template.gitignore --exclude new.sh --exclude test.sh --exclude .travis.yml . $1
+rsync -av --exclude .idea --exclude .git --exclude logs --exclude releases --exclude app.toml --exclude .gitignore --exclude new.sh --exclude test.sh --exclude .travis.yml . $1
+
+# 重命名包
+cd $1
+bash rename.sh $2
+rm rename.sh
 
 # 重命名app_template.toml
 mv app_template.toml app.toml
 
 # 重命名template.gitignore
 mv template.gitignore .gitignore
-
-# 重命名包
-cd $1
-bash rename.sh $2
-rm rename.sh
